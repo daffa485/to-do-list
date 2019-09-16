832ef733c7ffa767a3cd26fa96248590407976f6
daffa485 <sanamntzk96@gmail.com>
2019-09-16 20:42:35 +0700

    Firebase

--- src/main.js
+++ src/main.js
@@ -2,8 +2,24 @@ import Vue from 'vue'
 import App from './App.vue'
 import router from './router'
 import store from './store'
+import firebase from 'firebase'
 
 Vue.config.productionTip = false
+
+const config = {
+  apiKey: "AIzaSyBhhUdNuLXbPQ7vI5b3967iuBD93i0Vdqc",
+  authDomain: "to-do-list-f15cc.firebaseapp.com",
+  databaseURL: "https://to-do-list-f15cc.firebaseio.com",
+  projectId: "to-do-list-f15cc",
+  storageBucket: "",
+  messagingSenderId: "356660474949",
+  appId: "1:356660474949:web:1d55ed996a1bedab0c1f2c"
+
+}
+
+firebase.initializeApp(config)
+
+export const db = firebase.firestore()
 
 new Vue({
   router,
